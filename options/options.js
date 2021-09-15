4b6479a35694c6364e3a3e061585a9b0a2e1c295
Crowdbotics <team@crowdbotics.com>
2021-09-15 18:47:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "adastra_30582Identifier",
+  url: "https://adastra_30582Identifier.botics.co",
+  api: "https://adastra_30582Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
